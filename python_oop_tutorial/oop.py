1085829bc92fb17455994dce83a286fdb28a89dc
Bennett Levine <ben.lvn@gmail.com>
2019-04-28 02:19:40 -0400

    Did an OOP tutorial

---
+++ python_oop_tutorial/oop.py
@@ -0,0 +1,37 @@
+# Python Object-Oriented Programming
+
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@comapny.com'
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(emp_1.first, emp_1.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+        
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+# print(emp_1.email)
+# print(emp_2.email)
+
+# print(emp_1.fullname())
+
+# print(Employee.fullname(emp_1))
+
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+print(Employee.num_of_emps)
